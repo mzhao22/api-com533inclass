8153a581cfee99cb56579d441f778405958fb63d
Mia Mia <aceline0326@gmail.com>
2017-09-21 23:34:57 -0500

    make some final changes

--- site.js
+++ site.js
@@ -1,9 +1,10 @@
-var food = {
+var food = [
     {
 		"name": "Sweet Soy Chicken Wings",
 		"amount": "16",
     "type": "stir-fried",
 		"ingredients": [
+		{"name":"salt","amount": "1tspoon"},
 		"soysauce",
     "Chicken Wings",
     "sugar" ]},
@@ -20,21 +21,34 @@ var food = {
       "name": "Other materials",
       "materials": [
         "basil leaf 1",
-        "sesame 1 scoop"]
-		}
-  };
+        "sesame 1 scoop"
+      ]
+		}];
 
-$('#user-form').on("submit", function(e){
-	var food = $('#food').val();
-  $('#result').append('<li> food </li>');
-  e.preventDefault();
+ $('#user-form').on("submit", function(e){
+	 //var food = $('#food').val();
+     e.preventDefault();
+	 var foodname = $('#food').val();
+	 if ($('#food').val()==''){
+		 $('#result').append("<br />Here are the foods provided:<br />" + food[0].name + "");
+		 $('#result').append("<br />amount: " + food[0].amount + "");
+		 $('#result').append("<br />type: " + food[0].type + "");
+		 $('#result').append("<br />ingredients: " + food[0].ingredients[0].name + food[0].ingredients[0].amount + food[0].ingredients[1] + food[0].ingredients[2] + food[0].ingredients[3] + "");
+		 $('#result').append("<br />" + food[1].name + "");
+		 $('#result').append("<br />amount: " + food[1].count + "");
+		 $('#result').append("<br />type: " + food[1].type + "");
+		 $('#result').append("<br />ingredients: " + food[1].materials[0] + food[1].materials[1] + food[1].materials[2] + food[1].materials[3] + "");
+		 $('#result').append("<br />" + food[2].name + "");
+		 $('#result').append("<br />ingredients: " + food[2].materials[0] + food[2].materials[1] + "");
+	 }
+	 else {
+		 $('#result').append('<li> foodname </li>');
+		 $('#result').append("<li>" + foodname + "</li>");
+	 }
+	 console.log("Caught submit event");
+	 e.preventDefault();
+
 });
-
-$('#food').on('focus', function(){
-  console.log('')
-});
-
-
 
 
 //single food object or
